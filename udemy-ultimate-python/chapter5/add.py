56a3432a9eabf1e03f5c52e3499833d35e141021
Jay Clark <jay.clark@gmail.com>
2014-05-26 06:57:53 -0400

    chapter 5, exercise 1

---
+++ udemy-ultimate-python/chapter5/add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+print('Enter a number: ')
+number1 = int(raw_input())
+print('Enter another number: ')
+number2 = int(raw_input())
+print('The sum of these two numbers is: ' + str(number1+number2))
