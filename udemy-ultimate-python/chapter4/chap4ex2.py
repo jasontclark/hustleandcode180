6e95fd425ed13ed83ce9ef80cfb98033b51e809a
Jay Clark <jay.clark@gmail.com>
2014-05-26 06:33:05 -0400

    udemy-ultimate-python, chapter 4, exercise 2.

---
+++ udemy-ultimate-python/chapter4/chap4ex2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+grades = {'Raymond':92, 'Cynthia':83, 'Terrill':64, 'Jennifer':75, 'Clayton':88, 'Bryan':100, 'Danny':99}
+print grades['Raymond']
+print grades['Cynthia']
+print grades['Terrill']
+print grades['Jennifer']
+print grades['Clayton']
+print grades['Bryan']
+print grades['Danny']
