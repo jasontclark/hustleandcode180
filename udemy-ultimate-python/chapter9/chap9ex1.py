edeaefd0452b3d740d40ae85afc084e57e9f918f
Jay Clark <jay.clark@gmail.com>
2014-05-28 15:40:53 -0400

    removing duplicate code.

--- udemy-ultimate-python/chapter9/chap9ex1.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-""" Chapter 9, Exercise 1 """
-SQUARE = ((10, 8), (10, 23), (25, 23), (25, 8))
-SQUAREIT = iter(SQUARE)
-print next(SQUAREIT)
-print next(SQUAREIT)
-print next(SQUAREIT)
-print next(SQUAREIT)
