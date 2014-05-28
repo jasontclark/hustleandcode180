f172f8ba44c0a49b75c466ec854f73704336a6ca
Jay Clark <jay.clark@gmail.com>
2014-05-27 22:57:18 -0400

    udemy chapter 6, exercise 2

--- udemy-ultimate-python/chapter6/activity.py
+++ udemy-ultimate-python/chapter6/activity.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# TODO - Lecture 28 - CODE THIS UP
+message = 'The recommended activity is '
+print('Enter the temperature: ')
+temp = int(raw_input())
+
+if temp > 85:
+  message = message + 'swimming.'
+elif temp >= 70:
+  message = message + 'tennis.'
+elif temp >= 32:
+  message = message + 'golf.'
+elif temp >= 0:
+  message = message + 'dancing.'
+else:
+  message = message + 'sitting by the fire.'
+
+print(message)
