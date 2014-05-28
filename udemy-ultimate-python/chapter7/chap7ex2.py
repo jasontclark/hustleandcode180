8cbc5286f5c97cb86ba314ca99206c13030d4533
Jay Clark <jay.clark@gmail.com>
2014-05-27 23:35:27 -0400

    correcting file name in docstring

--- udemy-ultimate-python/chapter7/chap7ex2.py
+++ udemy-ultimate-python/chapter7/chap7ex2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-""" chap7ex1.py - udemy chapter 7, exercise 2 """
+""" chap7ex2.py - udemy chapter 7, exercise 2 """
 
 OP1 = 0.0
 OP2 = 0.0
@@ -9,11 +9,13 @@ OP = ''
 while OP1 != 'q':
     print 'Enter first number (q to quit): '
     OP1 = raw_input()
+    if OP1 == 'q':
+        break
+    OP1 = float(OP1)
     print 'Enter second number: '
-    OP2 = raw_input()
+    OP2 = float(raw_input())
     print 'Enter an operation (+,-,*,/): '
     OP = raw_input()
-
     if OP == '+':
         print OP1 + OP2
     elif OP == '-':
