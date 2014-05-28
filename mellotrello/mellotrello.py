abebc04d22f8fa6787cbb30a96d9a045ca220d4d
Jay Clark <jay.clark@gmail.com>
2014-05-28 15:32:27 -0400

    renaming the chapter folders for udemy. added a little bit for mellotrello too.

--- mellotrello/mellotrello.py
+++ mellotrello/mellotrello.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# mellotrello.py - terminal command that let's you see your untouched trello cards.
-#
+""" mellotrello.py - terminal command that let's you see your untouched trello cards. """
+import urllib
+import urllib2
 from keys import keys
+
+TRELLO_API_URL = ''
---
+++ udemy-ultimate-python/chapter10/listcomp.py
@@ -0,0 +1,3 @@
+N = range(1, 101)
+EN = [x for x in N if x % 2 == 0]
+print EN
