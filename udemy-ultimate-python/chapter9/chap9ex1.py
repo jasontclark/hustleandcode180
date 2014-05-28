d193324f305e21a06de1c8f564ccb5b5f471c753
Jay Clark <jay.clark@gmail.com>
2014-05-28 13:04:14 -0400

    Chapter 9, Exercise 1

---
+++ udemy-ultimate-python/chapter9/chap9ex1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" Chapter 9, Exercise 1 """
+SQUARE = ((10, 8), (10, 23), (25, 23), (25, 8))
+SQUAREIT = iter(SQUARE)
+print next(SQUAREIT)
+print next(SQUAREIT)
+print next(SQUAREIT)
+print next(SQUAREIT)
