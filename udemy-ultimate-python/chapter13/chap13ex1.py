4021159394209fa8ce6ee35298e9f726ce867b19
Jay Clark <jay.clark@gmail.com>
2014-05-30 23:46:08 -0400

    chapter 13, exercise 1

---
+++ udemy-ultimate-python/chapter13/chap13ex1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" chapter 13, exercise 1 """
+import math
+
+def square(number):
+    return number * number
+
+def sqrt(number):
+    return sqrtHelper(1.0, number)
+
+def sqrtHelper(guess, number):
+    if closeEnough(guess, number):
+        return guess
+    else:
+        return sqrtHelper(improve(guess, number), number)
+
+def closeEnough(guess, number):
+    return (math.fabs((square(guess)) - number ) < 0.001)
+
+def improve(guess, x):
+    return average(guess, (x / guess))
+
+def average(x, y):
+    return (x + y) / 2
+
+print(sqrt(144))
