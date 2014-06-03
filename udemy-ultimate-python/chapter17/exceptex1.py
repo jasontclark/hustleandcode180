e059adbc3e5e5d877b1aef55a94ca08b9b31ea7f
Jay Clark <jay.clark@gmail.com>
2014-06-02 22:03:14 -0400

    chapter 17, exercise 1

--- udemy-ultimate-python/chapter17/exceptex1.py
+++ udemy-ultimate-python/chapter17/exceptex1.py
@@ -11,18 +11,18 @@ def calc(op1, op2, op):
     elif op == '/':
         return op1 / op2
 
-cont = 'y'
-while cont != 'n':
+CONT = 'y'
+while CONT != 'n':
     print 'Enter the first number: '
     num1 = int(input())
     print 'Enter the second number: '
     num2 = int(input())
     print 'Enter operation: '
-    op = input()
+    op = str(input())
     if op == '/' and num2 == 0:
         print 'Cannot divide by zero.'
         continue
     else:
         print calc(num1, num2, op)
     print 'Do you want to continue (y/n) '
-    cont = input()
+    CONT = input()
