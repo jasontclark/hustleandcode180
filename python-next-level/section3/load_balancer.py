139891683ab053b09355ab91926392f785b9c465
Jay Clark <jay.clark@gmail.com>
2014-06-27 06:57:10 -0400

    another get_server method definition using generators.

--- python-next-level/section3/load_balancer.py
+++ python-next-level/section3/load_balancer.py
@@ -1,19 +1,37 @@
 #!/usr/bin/env python
 """ load_balancer.py - simulated load balancer """
 SERVERS = ['APP1', 'APP2', 'APP3']
+# SERVERS = ['APP1', 'APP2', 'APP3', 'APP4', 'APP5', 'APP6', 'APP7']
 
-n = -1
-
+# n = -1
 # def get_server():
 #     global n
 #     n += 1
 #     print SERVERS[n % len(SERVERS)]
 
-import itertools
-## infinite loop iterator
-cycle = itertools.cycle(SERVERS)
-num = 10
+# import itertools
+# cycle = itertools.cycle(SERVERS)
+# def get_server():
+#     global cycle
+#     return cycle.next()
 
-for i in num:
-    get_server
-    i -= 1
+def get_server():
+    def f():
+        while True:
+            i = SERVERS.pop(0)
+            SERVERS.append(i)
+            yield i
+    return next(f())
+
+# def get_server()
+#     try:
+#         return next(get_server.s)
+#     except StopIteration;
+#         get_server.s = iter(SERVERS)
+#         return next(get_server.s)
+# setattr(get_server, 's', iter(SERVERS))
+
+
+if __name__ == '__main__':
+    for i in range(9):
+        print get_server()
