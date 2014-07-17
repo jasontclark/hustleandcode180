60c37301fde00ef39a38e6b29ba306cad91a5ff3
Jay Clark <jay.clark@gmail.com>
2014-07-17 07:05:30 -0400

    adding some commenting to re_search.py

--- python-next-level/section4/re_search.py
+++ python-next-level/section4/re_search.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
-
+""" re_search.py - searches a string for a leading zero."""
 import re
 
 def noFirstZero(formula):
+    """ uses a regular expression search to look for a leading zero. """
     return not re.search(r'\b0[0-9]', formula)
 
 def test_noFirstZero(L):
+    """ runs a quick test. """
     for formula in L:
         print formula, noFirstZero(formula)
 
