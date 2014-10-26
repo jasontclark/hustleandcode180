93d4ba5b44a3081731b820cb50b1aae2e06e049b
Jay Clark <jay.clark@gmail.com>
2014-10-26 17:13:39 -0400

    more try/except examples

--- python-next-level/section4/try_except.py
+++ python-next-level/section4/try_except.py
@@ -5,11 +5,19 @@
 
 userDefined = 'b'
 
+class MyException(Exception):
+    pass
+
 try:
-    a = int(userDefined)
+    #a = int(userDefined)
+    raise MyException('My Exception Error')
 except ValueError:
     a = int(1)
-except Exception:
+    print 'Error: ', str(a)
+except MyException as e:
     a = int(0)
-finally:
+    print 'Error: ', str(a), str(e)
+except:
+    print 'Error -1'
+else:
     print a
