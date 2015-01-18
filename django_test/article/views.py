5a9f1db407bfaa828269eb3ac944ed981ab09cef
Jay Clark <jay.clark@gmail.com>
2015-01-17 22:59:11 -0500

    Adding the template code.

--- django_test/article/views.py
+++ django_test/article/views.py
@@ -1,8 +1,16 @@
 #from django.shortcuts import render
 from django.http import HttpResponse
+from django.template.loader import get_template
+from django.template.context import Context
 
 # Create your views here.
 def hello(request):
     name = "Jay"
     html = "<html><body>Hi %s. It worked.</body></html>" % name
     return HttpResponse(html)
+
+def hello_template(request):
+    name = "Jay"
+    t = get_template('hello.html')
+    html = t.render(Context({'name': name}))
+    
