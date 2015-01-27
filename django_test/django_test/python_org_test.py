b75effdac76c126c8d0456ee6a42edaae26d0d37
Jay Clark <jay.clark@gmail.com>
2015-01-26 22:41:50 -0500

    Chrome Driver test.
    Adding in the Chrome Driver. Added the full path to the test script.

--- django_test/django_test/python_org_test.py
+++ django_test/django_test/python_org_test.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome()
+driver = webdriver.Chrome("/Users/jclark/Code/Repos/git/github/hustleandcode180/django_test/chromedriver")
 driver.get("http://www.python.org")
 assert "Python" in driver.title
 elem = driver.find_element_by_name("q")
